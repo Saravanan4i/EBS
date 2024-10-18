72f3ff696f31ca0f0dd807becf73c262f7daeef6
Saravanan4i <saravanan.annamalaisamy@4iapps.com>
2024-10-18 15:56:03 +0530

    Add files via upload

---
+++ RECENT.sql
@@ -0,0 +1,9 @@
+CREATE TABLE RECENT (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2),
+    hire_date DATE
+/
+alter table RECENT add creation_by NUMBER;
