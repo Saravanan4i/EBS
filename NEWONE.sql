de84d081ec2714a048eea25986a837c9a30f590a
Saravanan4i <saravanan.annamalaisamy@4iapps.com>
2024-10-18 14:37:59 +0530

    Add files via upload

---
+++ NEWONE.sql
@@ -0,0 +1,9 @@
+-- Create the Members table
+CREATE TABLE NEWONE (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2),
+    hire_date DATE
+);
