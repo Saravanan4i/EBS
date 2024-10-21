64f65a6c9a3e62b2af3676dd5a21eb67014cf2b7
Saravanan4i <saravanan.annamalaisamy@4iapps.com>
2024-10-21 22:56:35 +0530

    Delete saravanan.sql

--- saravanan.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE saravanan (
-    first_name VARCHAR(50),           -- First name of the person
-    last_name VARCHAR(50),            -- Last name of the person
-    email VARCHAR(100),               -- Email address
-);
