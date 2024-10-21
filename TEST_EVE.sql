62588c28cbeda85dd8e0cd959d87b12f376b69ac
Saravanan4i <saravanan.annamalaisamy@4iapps.com>
2024-10-21 22:17:14 +0530

    Create TEST_EVE.sql

---
+++ TEST_EVE.sql
@@ -0,0 +1,8 @@
+CREATE TABLE TEST_EVE (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10, 2),
+    hire_date DATE );
+/
