9424a73752120d9dfc3fffe93f50008ab637f213
Saravanan4i <saravanan.annamalaisamy@4iapps.com>
2024-10-21 23:08:53 +0530

    Delete TESTINGFLOW.sql

--- TESTINGFLOW.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE TESTINGFLOW (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    department VARCHAR(50),
-    salary DECIMAL(10, 2),
-    hire_date DATE );
